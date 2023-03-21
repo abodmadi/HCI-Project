323ad44d2fad4239ca08a46a4de5ab1038471923
Abdullah Madi <85120273+abodmadi@users.noreply.github.com>
2023-03-21 10:12:20 +0200

    Add files via upload

---
+++ TUIO_Latest/TUIO11_NET-master/Video.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuioDemo1
+{
+    public partial class Video : Form
+    {
+        public Video(string path)
+        {
+            InitializeComponent();
+            //MessageBox.Show( TuioDemo.vvv.ToString());
+            MediaPlayer.URL = path;
+            MediaPlayer.Ctlcontrols.play();
+        }
+    }
+}
